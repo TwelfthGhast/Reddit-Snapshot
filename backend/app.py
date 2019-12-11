adb1755eef8be43bd4c045b2d06ae796da80932f
Dennis Ying <dennisyingswebpages@gmail.com>
2019-12-11 17:21:41 +1100

    WIP

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,13 @@
 import praw
+#import psycopg2
 
 CLIENT_ID = input("Enter Client ID:").strip()
 CLIENT_SECRET = input("Enter Client Secret:").strip()
 SUBREDDIT = input("Enter target subreddit:").strip()
 USER_AGENT = "Snapshot Tool v0.0.1 Built: 11 Dec 2019"
+# time_filter – Can be one of: all, day, hour, month, week, year (default: all).
+TIME_FILTER = "day"
+
 
 reddit = praw.Reddit(
     client_id=CLIENT_ID,
@@ -13,6 +17,9 @@ reddit = praw.Reddit(
 
 subreddit = reddit.subreddit(SUBREDDIT)
 
-print(subreddit.display_name)
-print(subreddit.title)
-print(subreddit.description)
+
+
+# PRAW api limits to top 1000
+for submission in subreddit.top(time_filter=TIME_FILTER, limit=1):
+    print(vars(submission))
+    #print(f"{submission.title}\t{submission.url}")
